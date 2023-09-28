330192a39d8a36d2cf227362df0bcadd97d43873
Ivan D. Velez <ivancho175@hotmail.com>
2023-09-28 11:40:59 -0500

    commit to fix the source

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       preloadingStrategy: PreloadAllModules,
+      useHash: true,
     }),
   ],
   exports: [RouterModule],
